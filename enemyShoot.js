67b59e91c4631514687a0eafba83cebcea2322e0
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2022-07-14 19:04:01 -0700

    Add files via upload

---
+++ enemyShoot.js
@@ -0,0 +1,68 @@
+AFRAME.registerComponent("enemy-bullets", {
+    init: function () {
+        setInterval(this.shootEnemyBullet, 2000)
+    },
+    shootEnemyBullet: function () {
+var els = document.querySelectorAll(".enemy");
+
+for( var i = 0; i < els.length; i++){
+    var enemyBullet = document.createElement("a-entity");
+    enemyBullet.setAttribute("geometry",{
+        primitive: "sphere",
+        radius: 0.2,
+
+    })
+    enemyBullet.setAttribute("material","color","#282b29");
+    var position = els[i].getAttribute("position")
+    
+    enemyBullet.setAttribute("position",{
+        x:position.x+1.5,
+        y: position.y+3.5,
+        z:position.z,
+
+    });
+    var scene = document.querySelector("#scene");
+    scene.appendChild(enemyBullet);
+
+    var position1 = new THREE.Vector3();
+    var position2 = new THREE.Vector3();
+
+    //setting the velocity and direction
+
+    var direction = new THREE.Vector3();
+    direction.subVectors(position1,position2).normalize();
+    enemyBullet.setAttribute("velocity",direction.multiplyScalar(10));
+
+    enemyBullet.setAttribute("dynamic-body",{
+        shape: "sphere",
+        mass: "0",
+        
+    });
+    var element = document.querySelector("#countLife");
+    var playerLife = parseInt(element.getAttribute("text").value);
+
+
+
+    var enemy = els[i].object3D;
+    var player= document.querySelector("#weapon").object3D;
+
+    player.getWorldPosition(position1);
+    enemy.getWorldPosition(position2);
+
+    //collide event on bullets
+    enemyBullet.addEventListener("collide",function(e){
+        if(e.detail.body.el.id==="weapon"){
+            if(playerLife> 0 ){
+                playerLife -= 1;
+                element.setAttribute("text",{
+                    value: playerLife,
+                })
+            }
+        }
+    })
+}
+
+        
+    },
+
+});
